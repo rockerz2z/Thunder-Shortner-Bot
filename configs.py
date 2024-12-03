68c92f03c513d18994f6039a02a1fdbcb4c7e79f
Techify Bots <RahulReviewsYT@gmail.com>
2024-12-03 15:30:32 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,22 +4,22 @@ API_ID = genv("API_ID", "")
 API_HASH = genv("API_HASH", "")
 BOT_TOKEN = genv("BOT_TOKEN", "")
 BASE_URL = genv("BASE_URL", "")
-SUPPORT_GROUP = genv("SUPPORT_GROUP", "CodeXsupport")
-UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "RahulReviewsYT")
+SUPPORT_GROUP = genv("SUPPORT_GROUP", "Techifysupport")
+UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "TechifyBots")
 DATABASE_URL = genv("DATABASE_URL", "")
 START_TXT = '''<b>Hᴇʟʟᴏ {}, I Aᴍ ShortLink Convetor Bot.
 ๏ I ᴄᴀɴ Cᴏɴᴠᴇʀᴛ ʏᴏᴜʀ ʟɪɴᴋs ᴛᴏ Sʜᴏʀᴛ ʟɪɴᴋs ᴜsɪɴɢ ʏᴏᴜʀ ᴀᴩɪ.
 ๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ Hᴇʟᴩ Mᴇɴᴜ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs.
-๏ Uᴘᴅᴀᴛᴇs - @RahulReviewsYT</b>'''
+๏ Uᴘᴅᴀᴛᴇs - @TechifyBots</b>'''
 HELP_TXT = '''Sᴇɴᴅ ᴍᴇ ᴀɴʏ ʟɪɴᴋ ɪ ᴡɪʟʟ ᴄᴏɴᴠᴇʀᴛ ɪᴛ ᴛᴏ sʜᴏʀᴛ ʟɪɴᴋ ᴜsɪɴɢ ʏᴏᴜʀ ᴀᴘɪ
 ʏᴏᴜ ᴄᴀɴ sʜᴀʀᴇ ᴛʜᴀᴛ ʟɪɴᴋ ᴀɴᴅ ᴇᴀʀɴ ᴍᴏɴᴇʏ.
-sᴇɴᴅ /ShortLink ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ - Uᴘᴅᴀᴛᴇs - @RahulReviewsYT'''
+sᴇɴᴅ /ShortLink ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ - Uᴘᴅᴀᴛᴇs - @TechifyBots'''
 ABOUT_TXT = '''<b>╔════❰ ShortLink Convetor ❱═══❍
 ║ ┏━━━━━━━━━❥
 ║ ┣ Mʏ ɴᴀᴍᴇ -> {}
-║ ┣ Mʏ Oᴡɴᴇʀ -> @CodeXBro
-║ ┣ Uᴘᴅᴀᴛᴇꜱ -> @RahulreviewsYT
-║ ┣ 𝖲ᴜᴘᴘᴏʀᴛ -> @CodeXsupport
+║ ┣ Mʏ Oᴡɴᴇʀ -> @CallOwnerBot
+║ ┣ Uᴘᴅᴀᴛᴇꜱ -> @TechifyBots
+║ ┣ 𝖲ᴜᴘᴘᴏʀᴛ -> @TechifySupport
 ║ ┣ ๏ Cʜᴇᴄᴋ ʜᴇʟᴘ ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ.
 ║ ┗━━━━━━━━━❥
 ╚═════❰ @ ❱═════❍</b>'''
