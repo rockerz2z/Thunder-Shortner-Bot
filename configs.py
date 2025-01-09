e1b31a21e0b13ea872be7f92329e615bdf671e4d
Rocker2k <119720415+rockerz2z@users.noreply.github.com>
2025-01-09 20:47:03 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,22 +4,22 @@ API_ID = genv("API_ID", "")
 API_HASH = genv("API_HASH", "")
 BOT_TOKEN = genv("BOT_TOKEN", "")
 BASE_URL = genv("BASE_URL", "")
-SUPPORT_GROUP = genv("SUPPORT_GROUP", "Techifysupport")
-UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "TechifyBots")
+SUPPORT_GROUP = genv("SUPPORT_GROUP", "Linkshortner_Support")
+UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "R2K_Bots")
 DATABASE_URL = genv("DATABASE_URL", "")
 START_TXT = '''<b>Hᴇʟʟᴏ {}, I Aᴍ ShortLink Convetor Bot.
 ๏ I ᴄᴀɴ Cᴏɴᴠᴇʀᴛ ʏᴏᴜʀ ʟɪɴᴋs ᴛᴏ Sʜᴏʀᴛ ʟɪɴᴋs ᴜsɪɴɢ ʏᴏᴜʀ ᴀᴩɪ.
 ๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ Hᴇʟᴩ Mᴇɴᴜ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs.
-๏ Uᴘᴅᴀᴛᴇs - @TechifyBots</b>'''
+๏ Uᴘᴅᴀᴛᴇs - @R2K_Bots</b>'''
 HELP_TXT = '''Sᴇɴᴅ ᴍᴇ ᴀɴʏ ʟɪɴᴋ ɪ ᴡɪʟʟ ᴄᴏɴᴠᴇʀᴛ ɪᴛ ᴛᴏ sʜᴏʀᴛ ʟɪɴᴋ ᴜsɪɴɢ ʏᴏᴜʀ ᴀᴘɪ
 ʏᴏᴜ ᴄᴀɴ sʜᴀʀᴇ ᴛʜᴀᴛ ʟɪɴᴋ ᴀɴᴅ ᴇᴀʀɴ ᴍᴏɴᴇʏ.
-sᴇɴᴅ /ShortLink ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ - Uᴘᴅᴀᴛᴇs - @TechifyBots'''
+sᴇɴᴅ /ShortLink ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ - Uᴘᴅᴀᴛᴇs - @R2K_Bots'''
 ABOUT_TXT = '''<b>╔════❰ ShortLink Convetor ❱═══❍
 ║ ┏━━━━━━━━━❥
 ║ ┣ Mʏ ɴᴀᴍᴇ -> {}
-║ ┣ Mʏ Oᴡɴᴇʀ -> @CallOwnerBot
-║ ┣ Uᴘᴅᴀᴛᴇꜱ -> @TechifyBots
-║ ┣ 𝖲ᴜᴘᴘᴏʀᴛ -> @TechifySupport
+║ ┣ Mʏ Oᴡɴᴇʀ -> @ProfessorR2K
+║ ┣ Uᴘᴅᴀᴛᴇꜱ -> @R2K_Bots
+║ ┣ 𝖲ᴜᴘᴘᴏʀᴛ -> @Linkshortner_Support
 ║ ┣ ๏ Cʜᴇᴄᴋ ʜᴇʟᴘ ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ.
 ║ ┗━━━━━━━━━❥
 ╚═════❰ @ ❱═════❍</b>'''
